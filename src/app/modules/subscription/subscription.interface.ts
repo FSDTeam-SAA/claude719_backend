76dd9fc447ae7c6f7105d7b39091ca0b1e5e3b76
Wrong1234 <mahabur24cse@gmail.com>
2025-12-17 10:32:06 +0600

    add subscription feature

---
+++ src/app/modules/subscription/subscription.interface.ts
@@ -0,0 +1,18 @@
+import { Types } from 'mongoose';
+
+export interface ISubscription {
+  numberOfGames: {
+    type: number;
+    required:true
+  },
+  price: {
+    type:number,
+    required:true
+  },
+  currency: string;
+  interval: 'monthly' | 'yearly';
+  features: string[];
+  isActive: boolean;
+  title: string;
+  totalSubscripeUser?: Types.ObjectId[];
+}
